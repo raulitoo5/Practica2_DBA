3456c6ac2c9bda2406e2e65c2f468e1a04177c6f
raulitoo5 <raulcruz@correo.ugr.es>
2023-11-02 10:24:43 +0100

    practica 2 funciona bien, falta meter comportamientos, mejorar estructura y mejorar movimiento (opcional)

--- src/practica2/Practica2.java
+++ src/practica2/Practica2.java
@@ -1,21 +1,61 @@
+///*
+// * To change this license header, choose License Headers in Project Properties.
+// * To change this template file, choose Tools | Templates
+// * and open the template in the editor.
+// */
+//package practica2;
+//import practica2.Mapa;
+///**
+// *
+// * @author yo
+// */
+//public class Practica2 {
+//
+//    /**
+//     * @param args the command line arguments
+//     */
+//    public static void main(String[] args) {
+//        String archivoMapa = "mapa.txt";
+//        System.out.println("mapa.txt");
+//        var mapa = new Mapa("mapa.txt");
+//        mapa.mostrarMapa();
+//    }
+//}
+
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package practica2;
+
 
 /**
  *
  * @author yo
  */
-public class Practica2 {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+public class Practica2 {
     
+    public static void main(String[] args) {        
+        
+        // Esto lo he quitado ya que el mapa se hace automáticamente cuando se crea el agente
+//        Agente agente = new Agente();
+        Agente agente = new Agente(9,9,0,0);
+        agente.marcarCasilla(agente.getX(), agente.getY());
+        //agente.colocarObjetivo();
+        agente.getMapa().mostrarMapa();
+
+        for (int i = 0; !agente.getObjetivo(); i++) {
+
+            System.out.println("---------------------------------------------------------------------------");
+            agente.MoverAgente();
+            agente.getMapa().mostrarMapa();
+            System.out.println("Posición del objetivo X: " + agente.getXobjetivo() + "positicón del objetivo Y: " + agente.getYobjetivo());
+            System.out.println("llevo: " + i + " movimientos");
+            System.out.println("posicion actual del agente: " + agente.getX() + " " + agente.getY());
+        }
+    }
 }
+
+
+
